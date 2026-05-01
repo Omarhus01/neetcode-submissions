a7cc93ba9bdffcc41c400be58f1472eb09c1cc0c
Omarhus01 <58533404+Omarhus01@users.noreply.github.com>
2026-05-01 01:25:10 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_s = {}
+        dict_t = {}
+
+        for char in s:
+            if char not in dict_s:
+                dict_s[char] = 0 
+            dict_s[char] += 1
+        
+        for chars in t:
+            if chars not in dict_t:
+                dict_t[chars] = 0
+            dict_t[chars] += 1
+        
+        return dict_s == dict_t
